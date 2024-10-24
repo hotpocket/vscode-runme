48abe7f4c80fff0eb64ebd670e4221583b7bf2cb
hotpocket <githotpocket@gmail.com>
2024-10-24 14:25:01 -0700

    remove old ref.

--- src/extension/kernel.ts
+++ src/extension/kernel.ts
@@ -152,7 +152,6 @@ export class Kernel implements Disposable {
 
     this.onVarsChangeEvent = new EnvVarsChangedEvent()
     this.#experiments.set('grpcSerializer', config.get<boolean>('grpcSerializer', true))
-    this.#experiments.set('connectSerializer', config.get<boolean>('connectSerializer', true))
     this.#experiments.set('grpcRunner', config.get<boolean>('grpcRunner', true))
     this.#experiments.set('grpcServer', config.get<boolean>('grpcServer', true))
     this.#experiments.set('smartEnvStore', config.get<boolean>('smartEnvStore', false))
